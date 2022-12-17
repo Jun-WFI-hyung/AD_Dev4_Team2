d11a077d014afbc9e8b75ec3e6721f3cd773fe13
Junhyung Kim <shyainkr@gmail.com>
2022-12-17 15:37:06 +0900

    fightinggit status!

---
+++ lane_detection/include/Simulator.h
@@ -0,0 +1,17 @@
+#ifndef SIMUL
+#define SIMUL
+
+#include <iostream>
+#include <vector>
+#include <ros/ros.h>
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+using namespace std;
+using namespace cv;
+using namespace cv_bridge;
+using namespace ros;
+
+
+
+#endif
